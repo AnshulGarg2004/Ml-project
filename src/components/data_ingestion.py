3cda734ddd48129556c66f2d6ea446e2b8956643
AnshulGarg2004 <ganshul2004@gmail.com>
2025-06-24 00:53:09 +0530

    Data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,43 @@
+import os 
+import sys
+from src.exception import Custon_exception
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class Data_ingestion_config:
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "raw.csv")
+
+class Data_ingestion:
+    def __init__(self):
+        self.ingestion_config = Data_ingestion_config()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or componenet")
+        try:
+            df = pd.read_csv('notebook/data/student.csv')
+            logging.info("Read the dataset from dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header = True)
+
+            train_set, test_set = train_test_split(df, test_size= 0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header=True)
+            train_set.to_csv(self.ingestion_config.test_data_path, index = False, header=True)
+
+            logging.info('Ingestion of the data completed')
+
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+    
+        except Exception as e:
+            raise Custon_exception(e, sys)
+        
+if __name__ == "__main__":
+    obj = Data_ingestion()
+    obj.initiate_data_ingestion()
